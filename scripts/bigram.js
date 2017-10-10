4f401f376315a18b6759f037476625bd09625d04
jmazrimas <joe.mazrimas@gmail.com>
2017-10-09 21:09:08 -0500

    working for mixed case

--- scripts/bigram.js
+++ scripts/bigram.js
@@ -7,6 +7,9 @@ module.exports = {
       var wordAhead = fileData[i+1];
       // there is a word ahead of this one
       if (wordAhead) {
+        // downcase both words for comparison
+        word = word.toLowerCase();
+        wordAhead = wordAhead.toLowerCase();
         histogram[word+" "+wordAhead] = histogram[word+" "+wordAhead] ? histogram[word+" "+wordAhead]+1 : 1
       }
     })
--- test/bigram.test.js
+++ test/bigram.test.js
@@ -13,7 +13,24 @@ describe('bigram', function() {
   });
 
   it('should return a single entry for two words', function() {
-    expect(bigram.returnHistogram(['foo','bar'])['foo bar']).to.be(1)
+    expect(bigram.returnHistogram(['foo','bar'])['foo bar']).to.be(1);
   });
+
+  it('should return 2 entries for a repeated histogram and only 1 for a single', function() {
+    expect(bigram.returnHistogram(['foo','bar','foo', 'bar'])['foo bar']).to.be(2);
+    expect(bigram.returnHistogram(['foo','bar','foo', 'bar'])['bar foo']).to.be(1);
+  });
+
+  it('should return 3 distinct entries for four words with no repeats', function() {
+    var bigramReturn = bigram.returnHistogram(['foo','bar','baz', 'qux']);
+    var numberOfEntries = Object.keys(bigramReturn).length;
+    expect(numberOfEntries).to.be(3);
+  });
+
+  it('should compare strings without case', function() {
+    expect(bigram.returnHistogram(['FoO','bar','FOo', 'Bar'])['foo bar']).to.be(2);
+    expect(bigram.returnHistogram(['FoO','bar','FOo', 'Bar'])['bar foo']).to.be(1);
+  });
+
 
 });
